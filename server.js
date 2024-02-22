dd2752420638501e945c6ecaa5ef3efeb6a1eefb
Aalishan17Raza <aalishan69raza@gmail.com>
2024-02-22 22:09:52 +0530

    added cors

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const authRouter = require('./routes/auth.js');
 const db = require('./models');
 const { config } = require('dotenv');
 config();
+const cors = require('cors');
 
 const app=express();
 const PORT=process.env.PORT || 3000;
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', authRouter);
 app.use('/product',productRoutes);
 app.use('/chat',chatRoutes);
