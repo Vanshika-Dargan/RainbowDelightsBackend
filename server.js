0bf56129fc3e1541863d960d3da8846f59d6c6b5
Aalishan17Raza <aalishan69raza@gmail.com>
2024-02-26 22:25:46 +0530

    made images static

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const app=express();
 const PORT=process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors())
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use('/api', authRouter);
 app.use('/product',uploadMiddleWare.single('image'),productRoutes);
